8ad2bd9ef8062205ab1fb9c957e6f1815f2962d4
vinhtq279 <vinhtq279@gmail.com>
2021-12-29 13:55:16 +0700

    .

--- PF2110R1/Case Study/script.js
+++ PF2110R1/Case Study/script.js
@@ -1,3 +1,11 @@
+/**
+ * Nguoi dung vao trang index, neu co cookie (da login) thi vao thang trang quiz, khong thi redirect sang trang login
+ * Nguoi dung vào trang login, nếu đăng nhập bằng user chưa có trong localStorage thì redirect sang trang register
+ * Nguoi dùng vào trang register, thông tin sẽ dc save vào localStorage.
+ * Người dùng vào trang quiz, nếu đã làm quiz (có cookie lưu point) thì sẽ dc thông báo điểm quiz đã làm. Chưa có cookie thì sẽ làm sau đó nhận kq & kq dc lưu vào localStorage
+ */
+
+
 class Question{
     constructor(id, context, answers){
         this.id = id;
@@ -18,19 +26,25 @@ class User{
     }
 }
 
-function save(_questions){
+
+
+var users = [];                 // Luu mang user
+var results = [['1'], ['2']];   // Dap an
+var questions = [];             // Mang luu tru cac cau hoi & mang dap an cua no
+
+function save(_questions){      // Save lai cac cau tra loi cua user de tinh diem & luu vao cookie
     let answers = [];
     for (let x = 0; x < _questions.length; x++){   
         answers.push([]);     
         for (let i = 0; i < _questions[x].answers.length; i++){
             let id = `${_questions[x].id}_${_questions[x].answers[i]}`
-            console.log(id);
             let answer = document.getElementById(id)
             if(answer.checked == true){
                 answers[x].push(answer.value);
             }
         }
     }
+    console.log(_questions);
     console.log(answers);
     return answers;
 }
@@ -38,28 +52,34 @@ function save(_questions){
 function check_result(_results){
     let point = 0;
     let answers = save(questions);
-    console.log(_results);
     for (let x = 0; x < answers.length; x++){
         if (_results[x].toString() == answers[x].toString()){
             point ++;
         }
     }
     alert("You are have " + point + " points");
+    setCookie("point", point, 1);   // Save diem cua user vao cookie
 }
 
-function load_question(){
+
+
+function load_question(_questions){
+    if (getCookie("point") != ""){
+        username = getCookie("username");
+        point = getCookie("point");
+        alert(username + " have " + point + " points");
+    }
     let str = "";
-    let questions = [];
     let answers1 = ["A. Vinh", "B. David", "C. Viny"];
     let question1 = new Question(1, "What's your name?", answers1);
     let answers2 = ["A. 19", "B. 35", "C. 30"];
     let question2 = new Question(2, "How old are you?", answers2);
-    results = [['1'], ['2']];
-    questions.push(question1);
-    questions.push(question2);
+    
+    _questions.push(question1);
+    _questions.push(question2);
 
     // List question
-    for (const x of questions){
+    for (const x of _questions){
         str += x.context + "<br>";
         for (let i = 0; i < x.answers.length; i++){
             let str1 = `<input type='checkbox' id='${x.id}_${x.answers[i]}' value='${i + 1}'>${x.answers[i]}</input><br>`;
@@ -86,16 +106,11 @@ function getCookie(cname){
         let c = ca[i];
         
         while (c[0] == " "){
-            //console.log(c);
             c = c.substring(1);
-            //console.log(c);
         }
-        //console.log(c);
         if (c.indexOf(name) == 0){
             return c.substring(name.length, c.length);
-            console.log(c);
         }  
-        
     }
     return "";
 }
@@ -111,18 +126,24 @@ function index(){
             }
             else{
                 alert(user.username + user.password);
-            }   
-            console.log(user);      
+            }       
         });
-        console.log(users);
     }else {
         console.log("go to login");
         //window.location.replace("http://127.0.0.1:5500/PF2110R1/Case%20Study/login.html");
     }
-    console.log(getCookie("username"));
 }
 
 function login(){
+    if (getCookie("username") != ""){
+        users.forEach(user => {
+            // Have logined
+            if (getCookie("username") == user.username && getCookie("password") == user.password){
+                console.log("go to quiz");
+                //window.location.replace("http://127.0.0.1:5500/PF2110R1/Case%20Study/quiz.html");
+            }  
+        });
+    }
     users = JSON.parse(localStorage.getItem('users') || "[]");
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
@@ -142,21 +163,6 @@ function login(){
     }
 }
     
-    
-
-let users = [];
-let results = [];
-
-function create_users(){    
-    let user1 = new User("vinhtq", "123@123a");
-    let user2 = new User("quangtv", "123@123a@");
-    users.push(user1);
-    users.push(user2);
-}
-
-function debug(){
-    console.log(users);
-}
 
 function register(){
     users = JSON.parse(localStorage.getItem('users') || "[]");
@@ -167,4 +173,3 @@ function register(){
     localStorage.setItem('users', JSON.stringify(users));
     console.log("go to login");
 }
-
