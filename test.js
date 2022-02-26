7678dd44ca8dc59632fe363482ae310ca2e2ae2c
UCT <vinhtq279@gmail.com>
2022-02-26 09:35:57 +0700

    .

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("vinh");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Car(props) {
+  return <h2>I am a {props.color} Car!</h2>;
+}
+
+ReactDOM.render(<Car color="red"/>, document.getElementById('root'));
